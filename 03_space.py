31d2714ac7241e97765bc8f80d5ca7f7d9fbddd7
Will Sutton <sutton_999@hotmail.com>
2022-01-05 18:01:03 +0000

    updating text, readme and index

--- 03_space.py
+++ 03_space.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import random as rn
 
+# adding motherships at top band
 num_of_motherships = rn.randint(3,9)
 size_ms = [rn.randint(1,5) for p in range(0, num_of_motherships)]
 x_ms = [rn.uniform(0, 10) for p in range(0, num_of_motherships)]
@@ -19,18 +20,21 @@ ms = {'shape': shape_ms, 'size': size_ms,'colour': colour_ms,'x':x_ms,'y':y_ms}
 mothership_df = pd.DataFrame(data=ms)
 print(mothership_df)
 
+# create band of enemies
 num_of_enemies = rn.randint(70,120)
 size_em = [rn.randint(1,5) for p in range(0, num_of_enemies)]
 x_em = [rn.uniform(0, 10) for p in range(0, num_of_enemies)]
 y_em = [rn.uniform(3, 8) for p in range(0, num_of_enemies)]
 colour_em = [rn.randint(1,10) for p in range(0, num_of_enemies)]
 shape_em = ['enemy1_1','enemy2_1','enemy3_1','enemy1_2','enemy2_2','enemy3_2'] * num_of_enemies
+# randomise enemies
 rn.shuffle(shape_em)
 shape_em = shape_em[0:num_of_enemies]
 em = {'shape': shape_em, 'size': size_em,'colour': colour_em,'x':x_em,'y':y_em}
 enemy_df = pd.DataFrame(data=em)
 print(enemy_df)
 
+# add band of lasers and explosions
 num_of_lasers = rn.randint(10,20)
 size_ls = [rn.randint(1,2) for p in range(0, num_of_lasers)]
 x_ls = [rn.uniform(2, 8) for p in range(0, num_of_lasers)]
@@ -42,6 +46,7 @@ shape_ls = shape_ls[0:num_of_lasers]
 ls = {'shape': shape_ls, 'size': size_ls,'colour': colour_ls,'x':x_ls,'y':y_ls}
 laser_df = pd.DataFrame(data=ls)
 
+# add human ship
 num_of_humans = 1
 size_hm = [1]
 x_hm = [5]
@@ -51,6 +56,7 @@ shape_hm = ['ship']
 hm = {'shape': shape_hm, 'size': size_hm,'colour': colour_hm,'x':x_hm,'y':y_hm}
 human_df = pd.DataFrame(data=hm)
 
+# combine all data frames
 df = pd.concat([mothership_df,enemy_df,laser_df,human_df])
 
 # write data to csv
